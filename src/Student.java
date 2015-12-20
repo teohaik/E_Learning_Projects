c6217ea4d2566d17305881958ac509961952f51d
Theodore <teohaik@gmail.com>
2015-12-20 20:38:42 +0200

    Added new attribute in Student class

--- src/Student.java
+++ src/Student.java
@@ -2,10 +2,21 @@
 public class Student {
 
     private String name;
+	
+	private String code;
 
     public Student(String aName){
         name = aName;
     }
+	
+	public Student(String aName, String aCode){
+		name = aName;
+		code = aCode;
+	}
+	
+	public String toString(){
+		return "Student "+name+" - "+code;
+	}
 
 
 }
