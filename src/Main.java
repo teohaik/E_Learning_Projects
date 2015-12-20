a99f5c069f35c3472d1926a9f0de58bd434758b7
Theodore Chaikalis <teohaik@gmail.com>
2015-12-20 20:23:43 +0200

    Add some printouts in Main Added student class

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
 
+        System.out.println("This is a printout");
 
-
-
+        System.out.println("In this lesson we learn about HashCode, equals and GIT !!");
 
 
     }
---
+++ src/Student.java
@@ -0,0 +1,11 @@
+
+public class Student {
+
+    private String name;
+
+    public Student(String aName){
+        name = aName;
+    }
+
+
+}
